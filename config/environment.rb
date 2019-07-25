2e24eebe942b6e3bbd9a9e54cec911cce7016074
Alethea Cary <aletheacary@gmail.com>
2019-07-25 00:30:26 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 ENV['SINATRA_ENV'] ||= "development"
 
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
 configure :development do 
   set :database, 'sqlite3:db/development.db'
 end
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
